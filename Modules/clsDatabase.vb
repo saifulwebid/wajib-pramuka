09b904abe4a995f675d218849506f09369273283
Muhammad Saiful Islam <muhammad@saiful.web.id>
2014-01-17 05:18:46 +0700

    Create Database module for SQLite moduling purposes

---
+++ Modules/clsDatabase.vb
@@ -0,0 +1,25 @@
+﻿Namespace WajibPramuka
+    Module Database
+        ''' <summary>
+        ''' Connection string for SQLiteConnection.
+        ''' </summary>
+        Public ConnectionString As String = "data source=WP.db"
+
+        ''' <summary>
+        ''' Initialize a connection instance for the application.
+        ''' </summary>
+        ''' <returns>An instance of SQLiteConnection.</returns>
+        Public Function InitializeConnection() As SQLiteConnection
+
+            'Initialize a new SQLiteConnection instance.
+            Dim Connection As New SQLiteConnection
+
+            'Set up SQLiteConnection defaults.
+            Connection.ConnectionString = ConnectionString
+            Connection.DefaultTimeout = 5
+
+            'Return the instance.
+            Return Connection
+        End Function
+    End Module
+End Namespace
