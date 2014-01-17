1705818899ad46cbc63b1cbc0b3a5b6e5ed47764
Muhammad Saiful Islam <muhammad@saiful.web.id>
2014-01-17 17:21:17 +0700

    Create cryptography module for hashing purposes

---
+++ Modules/clsCryptography.vb
@@ -0,0 +1,18 @@
+﻿Namespace WajibPramuka
+    Module Cryptography
+        Public Function HashPassword(ByVal username As String, ByVal password As String) As String
+            Dim PasswordByte() As Byte
+            Dim SHA As New System.Security.Cryptography.SHA1CryptoServiceProvider
+            Dim PasswordString As String = ""
+            Dim PasswordOrigin As String = username & "_" & password
+
+            PasswordByte = SHA.ComputeHash(System.Text.Encoding.ASCII.GetBytes(PasswordOrigin))
+
+            For Each b As Byte In PasswordByte
+                PasswordString += b.ToString("x2")
+            Next
+
+            Return PasswordString
+        End Function
+    End Module
+End Namespace
